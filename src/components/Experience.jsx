7c379446b304162752014a67a28a14c4200763a4
TatoZhvania <tatojvania.03@gmail.com>
2024-11-08 02:27:31 +0400

    implementing Experience component

---
+++ src/components/Experience.jsx
@@ -0,0 +1,99 @@
+/* eslint-disable react/no-unescaped-entities */
+const Experience = () => {
+  return (
+    <div name="works" className="w-full md:screen text-gray-300 pt-[200px] ">
+      <div className="max-w-[1000px]  mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p
+            className="font-bold text-[#9f38f9] text-5xl border-b-4 border-[#00d8ff] max-w-[400px] mb-2"
+            data-aos="fade-right"
+            data-aos-delay="500"
+            data-aos-once="false"
+          >
+            Work Experiance
+          </p>
+        </div>
+        <div className="flex flex-col gap-2  py-6">
+          <div>
+            <h1 className="font-bold italic text-3xl border-b-4  max-w-[850px] mb-2">
+              Senior Service Monitoring Specialist / Jun 2024 - Present
+            </h1>
+          </div>
+          <div>
+            <p>
+              ⚪My responsibilities include actively monitoring through Zabbix
+              and Grafana, tracking system performance, identifying anomalies,
+              and coordinating with internal teams and external providers to
+              address and resolve product defects, technical updates, and any
+              service-related concerns. I manage and monitor automated scripts
+              using Python, working closely with department to investigate and
+              troubleshoot issues as they arise. Additionally, I perform
+              thorough testing for new products, promotions, and updates as they
+              go live. My technical expertise includes working with Docker for
+              containerization and Linux environments. While I monitor Zabbix
+              daily to ensure all systems are functioning optimally, I focus
+              primarily on operational oversight rather than on the technical
+              configuration aspects. This blend of monitoring, testing, and
+              cross-functional communication supports our mission to deliver a
+              seamless experience to our users and stakeholders.
+            </p>
+          </div>
+
+          <div>
+            <h1 className="font-bold italic text-3xl border-b-4  max-w-[960px] mb-2">
+              Junior Service Monitoring Specialist / Jan 2024 - Jun 2024 • 6 mon
+            </h1>
+          </div>
+          <div>
+            <p>
+              ⚪Monitoring and maintaining the functionality of products and
+              services on the website, identifying, reporting, and resolving
+              issues using Jira, including follow-up on incidents and error
+              handling with external providers. Communicating proactively with
+              internal teams and external providers to address product defects,
+              technical updates, and improvements. Ensuring continuous
+              monitoring of automated scripts and coordinating with developers
+              to troubleshoot and investigate issues. Conducting testing for new
+              products, promotions, and updates post-production, verifying
+              functionality and promotional mechanics. Collaborating with
+              Quality Assurance (QA) to ensure comprehensive product and site
+              performance, documenting and registering production bugs, and
+              reporting incidents promptly for quick resolution.
+            </p>
+          </div>
+
+          <div>
+            <h1 className="font-bold italic text-3xl border-b-4  max-w-[620px] mb-2">
+              KYC AGENT / Oct 2022 - Dec 2022 • 3 mos
+            </h1>
+          </div>
+          <div>
+            <p>
+              ⚪Ensure compliance with regulatory requirements by verifying the
+              identity of customers and assessing the risk associated with their
+              accounts. Review customer documentation to validate their
+              authenticity and accuracy.
+            </p>
+          </div>
+
+          <div>
+            <h1 className="font-bold italic text-3xl border-b-4  max-w-[520px] mb-2">
+              CUSTOMER SERVICE SPECIALIST / Jun 2022 - Oct 2022 • 5 mos
+            </h1>
+          </div>
+          <div>
+            <p>
+              ⚪Provide exceptional service to customers by addressing their
+              inquiries, resolving issues, and ensuring their overall
+              satisfaction. Effectively communicate with customers through
+              various channels such as email, and chat, demonstrating strong
+              interpersonal and problem-solving skills.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
